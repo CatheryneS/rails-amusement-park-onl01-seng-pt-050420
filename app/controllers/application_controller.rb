8d5b84282959d37536eb85cc4ed72d9c677bb767
Catheryn Small <catheryn.small@gmail.com>
2020-10-15 18:29:16 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper
-  
   def current_user
-      session[:user_id] ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,18 +1,41 @@
 class AttractionsController < ApplicationController
+  before_action :find_attraction, only: [:show, :edit, :update]
   def index
     @attractions = Attraction.all
+    @user = current_user
   end
 
   def new
+    @attraction = Attraction.new
   end
 
   def create
+    @attraction = Attraction.create(ride_params)
+    redirect_to attraction_path(@attraction)
   end
 
   def show
-    @attraction = Attraction.find(params[:id])
+    @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+    @attraction.update(ride_params)
+    redirect_to attraction_path(@attraction)
   end
 
   def destroy
+  end
+
+  private
+
+  def ride_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
+  end
+
+  def find_attraction
+    @attraction = Attraction.find(params[:id])
   end
 end
